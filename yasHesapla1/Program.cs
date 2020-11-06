5e4af5c94710ac7bf467a4f8e2499c0979db8783
IR0NNTi <IR0NNTi@DESKTOP-960OI32>
2020-11-07 02:15:59 +0300

    YasHesapla

---
+++ yasHesapla1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace yasHesapla1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Doğum Tarihi Giriniz: ");
+            int tarih = Convert.ToInt32(Console.ReadLine());
+            int yas;
+            yas = 2020 - tarih;
+            Console.WriteLine(yas+" yaşındasın.");
+            Console.ReadLine();
+        }
+    }
+}
